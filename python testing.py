a9917058e22b8964576127a014c4eb459edf886f
Spencerhill9 <spencer.hill@generatedhealth.com>
2025-04-16 16:54:31 -0400

    Update python testing.py
    Even more detail

--- python testing.py
+++ python testing.py
@@ -1,4 +1,5 @@
 name = input("What’s your name? ")
 age = input("How old are you? ")
 color = input("What's your favorite color? ")
-print("Hello " + name + "! You are " + age + " years old and your favorite color is " + color +)
+sex = input("What's your sex?")
+print("Hello " + name + "! You are " + age + " years old and are a " + sex + "and your favorite color is " + color +)
